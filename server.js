2a91f5da9f559cf319e807f23cc674301d3a9954
ARod <ajr.rodriguez095@gmail.com>
2022-03-31 21:02:33 -0400

    added helper functions and partials

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const session = require('express-session');
 
@@ -18,7 +19,7 @@ const sess = {
 };
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 
 
